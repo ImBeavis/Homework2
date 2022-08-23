fcf7eb1319595af057f3c16d95664777b60b0987
Andrey <Plyakin.andrei@yandex.ru>
2022-08-23 19:51:02 +0400

    задача34

---
+++ Seminar5/a_task_34/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Введите размер массива: ");
+int num = int.Parse(Console.ReadLine());
+
+int[] ArrResult(int size)
+{
+    int[] MyArray = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        MyArray[i] = new Random().Next(100, 1000);
+    }
+    return MyArray;
+}
+
+int[] array = ArrResult(num);
+Console.WriteLine(String.Join(" ", array));
+
+int PositiveNumber(int[] arr)
+{
+    int countPositive = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            countPositive += 1;
+        }
+    }
+    return countPositive;
+}
+Console.Write($"Количество четных чисел в массиве: {PositiveNumber(array)}");
