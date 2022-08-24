939fcca9a95a37d8bb6533a66cb7ec80edbbb033
Andrey <Plyakin.andrei@yandex.ru>
2022-08-24 20:14:12 +0400

    задача38

---
+++ Seminar5/a_task_38/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.Write("Введите размер массива: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("От: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("До: ");
+int numB = int.Parse(Console.ReadLine());
+
+double[] ArrResult(int size, int a, int b)
+{
+    double[] MyArray = new double[size];
+    for (int i = 0; i < size; i++)
+    {
+        MyArray[i] = new Random().Next(a, b);
+    }
+    return MyArray;
+}
+
+double[] array = ArrResult(num, numA, numB);
+Console.WriteLine(String.Join(" ", array));
+
+double MaxNum(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+Console.WriteLine($"Максимальный элемент массива: {MaxNum(array)}");
+
+double MinNum(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+Console.WriteLine($"Минимальный элемент массива: {MinNum(array)}");
+Console.WriteLine($"Разница между максимальным и минимальным: {(MaxNum(array) - MinNum(array))}");
