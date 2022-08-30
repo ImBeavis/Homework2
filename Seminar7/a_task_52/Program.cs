668a0e59151cd579b173685cf7033b4515b8ed42
Andrey <Plyakin.andrei@yandex.ru>
2022-08-30 19:08:01 +0400

    задача52

---
+++ Seminar7/a_task_52/Program.cs
@@ -0,0 +1,46 @@
+﻿Console.Write("Введите кол-во строк: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            result[i, j] = new Random().Next(1, 100 + 1);
+
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+            Console.Write($"{array[i, j]} ");
+        Console.WriteLine();
+    }
+}
+
+double Average(int[,] array)
+{
+    double result = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            result += array[j, i];
+        }
+        Console.WriteLine($"{result / array.GetLength(0):F2}");
+        result = 0;
+    }
+    return result;
+}
+
+
+int[,] myArray = GetArray(m, n);
+PrintArray(myArray);
+Console.WriteLine();
+Average(myArray);
