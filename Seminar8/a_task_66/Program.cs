ae419603f8c26dbe3f8d3f22cca89e0a27b1b073
Andrey <Plyakin.andrei@yandex.ru>
2022-09-03 19:08:36 +0400

    задача66

---
+++ Seminar8/a_task_66/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число от: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int PrintNumbers(int k, int n)
+{
+    if (k == n)
+    {
+        return k;
+    }
+    int sum = 0;
+    return  sum = k + PrintNumbers(k + 1, n);
+}
+
+Console.Write($"M = {M}, N = {N} -> {PrintNumbers(M, N)}");
