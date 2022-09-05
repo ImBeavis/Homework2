0eca9a0ee699a7587f2d96f871c5a16169687a3d
Andrey <Plyakin.andrei@yandex.ru>
2022-09-05 19:40:41 +0400

    задача68

---
+++ Seminar9/a_task_68/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число M: ");
+double M = double.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+double N = double.Parse(Console.ReadLine());
+
+double A(double n, double m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return A(n - 1, 1);
+    else
+        return A(n - 1, A(n, m - 1));
+}
+
+A(M, N);
+Console.Write(A(M, N));
